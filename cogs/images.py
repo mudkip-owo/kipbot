ada8806b8d29081dcee40bd967e46e07002c2c09
mudkip-owo <71028564+mudkip-owo@users.noreply.github.com>
2020-10-22 14:10:22 +0300

    Update images.py

--- cogs/images.py
+++ cogs/images.py
@@ -10,26 +10,36 @@ class images(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+
     @commands.command()
-    async def dog(self, ctx):
+    async def dog(self,ctx):
       url = 'https://some-random-api.ml/img/dog'
       result_url = requests.get(url)
       resultjson=result_url.json()
-      await ctx.send(resultjson['link'])
+      embed = discord.Embed(title="here is your dog.")
+      embed.set_image(url=resultjson['link'])
+      await ctx.send(embed=embed)
+
 
     @commands.command()
-    async def meme(self, ctx):
-      url = 'https://meme-api.herokuapp.com/gimme/dankmemes'
-      result_url = requests.get(url)
-      resultjson=result_url.json()
-      await ctx.send(resultjson['url'])
-
-    @commands.command()
-    async def cat(self, ctx):
+    async def cat(self,ctx):
       url = 'https://some-random-api.ml/img/cat'
       result_url = requests.get(url)
       resultjson=result_url.json()
-      await ctx.send(resultjson['link'])
+      embed = discord.Embed(title="here is your cat.")
+      embed.set_image(url=resultjson['link'])
+      await ctx.send(embed=embed)
+
+    @commands.command()
+    async def meme(self,ctx):
+      url = 'https://meme-api.herokuapp.com/gimme/dankmemes'
+      result_url = requests.get(url)
+      resultjson=result_url.json()
+      embed = discord.Embed(title=resultjson['title'])
+      embed.set_image(url=resultjson['url'])
+      await ctx.send(embed=embed)
+
+
 
 def setup(client):
     client.add_cog(images(client))
