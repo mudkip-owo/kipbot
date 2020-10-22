077a9adc3f8f806bcf118a0f4871dd71d965086a
mudkip-owo <71028564+mudkip-owo@users.noreply.github.com>
2020-10-22 14:26:33 +0300

    Its been a long time.

--- cogs/fun.py
+++ cogs/fun.py
@@ -7,8 +7,10 @@ import aiohttp
 import re
 from io import BytesIO
 from discord.ext import commands
+import json
 
-class Fun_Commands(commands.Cog):
+
+class fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -20,28 +22,48 @@ class Fun_Commands(commands.Cog):
 
     @commands.command()
     async def f(self, ctx, *, text: commands.clean_content = None):
-        """ Press F to pay respect """
+        """ Pay your respects """
         hearts = ['❤', '💛', '💚', '💙', '💜']
         reason = f"for **{text}** " if text else ""
         await ctx.send(f"**{ctx.author.name}** has paid their respect {reason}{random.choice(hearts)}")
 
 
+    @commands.command(aliases=["8ball", "eightball"])
+    async def _8ball(self, ctx, *, question):
+        responses = ["🎱It is certain.",
+                     "🎱It is decidedly so.",
+                     "🎱Without a doubt.",
+                     "🎱Yes - definitely.",
+                     "🎱You may rely on it.",
+                     "🎱As I see it, yes.",
+                     "🎱Most likely.",
+                     "🎱Outlook good.",
+                     "🎱Yes.",
+                     "🎱Signs point to yes.",
+                     "🎱Reply hazy, try again.",
+                     "🎱Ask again later.",
+                     "🎱Better not tell you now.",
+                     "🎱Cannot predict now.",
+                     "🎱Concentrate and ask again.",
+                     "🎱Don't count on it.",
+                     "🎱My reply is no.",
+                     "🎱My sources say no.",
+                     "🎱Outlook not so good.",
+                     "🎱Very doubtful."]
+        await ctx.send(random.choice(responses))
+
 
 
     @commands.command()
     async def reverse(self, ctx, *, text: str):
-        """ !poow ,ffuts esreveR
-        Everything you type after reverse will of course, be reversed
-        """
+        """ esreveR """
         t_rev = text[::-1].replace("@", "@\u200B").replace("&", "&\u200B")
-        await ctx.send(f"{t_rev}")
+        embed = discord.Embed(title='Ophelia Reverse!', description=t_rev)
+        await ctx.send(embed=embed)
 
     @commands.command()
     async def password(self, ctx, nbytes: int = 18):
-        """ Generates a random password string for you
-        This returns a random URL-safe text string, containing nbytes random bytes.
-        The text is Base64 encoded, so on average each byte results in approximately 1.3 characters.
-        """
+        """ Generates a random password string for you in dms """
         if nbytes not in range(3, 1401):
             return await ctx.send("I only accept any numbers between 3-1400")
         if hasattr(ctx, 'guild') and ctx.guild is not None:
@@ -55,81 +77,43 @@ class Fun_Commands(commands.Cog):
         await ctx.send(f"I'd rate `{thing}` a **{round(rate_amount, 4)} / 100**")
 
     @commands.command()
-    async def beer(self, ctx, user: discord.Member = None, *, reason: commands.clean_content = ""):
-        """ Give someone a beer! 🍻 """
+    async def coffee(self, ctx, user: discord.Member = None, *, reason: commands.clean_content = ""):
+        """ Give someone a coffee (or drink it yourself) """
         if not user or user.id == ctx.author.id:
-            return await ctx.send(f"**{ctx.author.name}**: paaaarty!🎉🍺")
+            return await ctx.send(f"**{ctx.author.name}**: Drank a cup of coffee☕ ")
         if user.id == self.bot.user.id:
-            return await ctx.send("*drinks beer with you* 🍻")
+            return await ctx.send("*i drink coffee with you* ☕")
         if user.bot:
-            return await ctx.send(f"I would love to give beer to the bot **{ctx.author.name}**, but I don't think it will respond to you :/")
+            return await ctx.send(f"I would love to give coffee to the bot **{ctx.author.name}**, but I don't think it would respond...")
 
-        beer_offer = f"**{user.name}**, you got a 🍺 offer from **{ctx.author.name}** (react to accept)"
-        beer_offer = beer_offer + f"\n\n**Reason:** {reason}" if reason else beer_offer
-        msg = await ctx.send(beer_offer)
+        coffee_offer = f"**{user.name}**, you got a ☕ offer from **{ctx.author.name}** (react to accept)"
+        cofeee_offer = coffe_offer + f"\n\n**Reason:** {reason}" if reason else coffee_offer
+        msg = await ctx.send(coffee_offer)
 
         def reaction_check(m):
-            if m.message_id == msg.id and m.user_id == user.id and str(m.emoji) == "🍻":
+            if m.message_id == msg.id and m.user_id == user.id and str(m.emoji) == "☕":
                 return True
             return False
 
         try:
-            await msg.add_reaction("🍻")
+            await msg.add_reaction("☕")
             await self.bot.wait_for('raw_reaction_add', timeout=30.0, check=reaction_check)
-            await msg.edit(content=f"**{user.name}** and **{ctx.author.name}** are enjoying a lovely beer together 🍻")
+            await msg.edit(content=f"**{user.name}** and **{ctx.author.name}** are enjoying a lovely coffee together☕")
         except asyncio.TimeoutError:
             await msg.delete()
-            await ctx.send(f"well, doesn't seem like **{user.name}** wanted a beer with you **{ctx.author.name}** ;-;")
+            await ctx.send(f"well, doesn't seem like **{user.name}** wanted a cup of coffee with you **{ctx.author.name}**")
         except discord.Forbidden:
             # Yeah so, bot doesn't have reaction permission, drop the "offer" word
-            beer_offer = f"**{user.name}**, you got a 🍺 from **{ctx.author.name}**"
-            beer_offer = beer_offer + f"\n\n**Reason:** {reason}" if reason else beer_offer
-            await msg.edit(content=beer_offer)
-
-    @commands.command(aliases=['howhot', 'hot'])
-    async def hotcalc(self, ctx, *, user: discord.Member = None):
-        """ Returns a random percent for how hot is a discord user """
-        user = user or ctx.author
-
-        random.seed(user.id)
-        r = random.randint(1, 100)
-        hot = r / 1.17
-
-        emoji = "💔"
-        if hot > 25:
-            emoji = "❤"
-        if hot > 50:
-            emoji = "💖"
-        if hot > 75:
-            emoji = "💞"
-
-        await ctx.send(f"**{user.name}** is **{hot:.2f}%** hot {emoji}")
-
-    @commands.command(aliases=['gaycalc', 'geir8'])
-    async def howgay(self, ctx, *, user: discord.Member = None):
-        """ Returns a random percent for how hot is a discord user """
-        user = user or ctx.author
-
-        random.seed(user.id)
-        r = random.randint(1, 100)
-        gay = r / 1.17
-
-        emoji = "🌈"
-        if gay > 25:
-            emoji = "🌈"
-        if gay > 50:
-            emoji = "🌈"
-        if gay > 75:
-            emoji = "🌈"
-
-        await ctx.send(f"**{user.name}** is **{gay:.2f}%** gay{emoji}")
+            coffee_offer = f"**{user.name}**, you got a ☕ from **{ctx.author.name}**"
+            cofeee_offer = coffee_offer + f"\n\n**Reason:** {reason}" if reason else coffee_offer
+            await msg.edit(content=coffee_offer)
 
 
     @commands.command(aliases=['slots', 'bet'])
     @commands.cooldown(rate=1, per=3.0, type=commands.BucketType.user)
     async def slot(self, ctx):
-        """ Roll the slot machine """
-        emojis = "🍎🍊🍐🍋🍉🍇🍓🍒"
+        """ Roll a slot machine """
+        emojis = "🟥🟦🟩🟪🟧🟨⬜🟧"
         a = random.choice(emojis)
         b = random.choice(emojis)
         c = random.choice(emojis)
@@ -137,12 +121,18 @@ class Fun_Commands(commands.Cog):
         slotmachine = f"**[ {a} {b} {c} ]\n{ctx.author.name}**,"
 
         if (a == b == c):
-            await ctx.send(f"{slotmachine} All matching, you won! 🎉")
+            await ctx.send(f"{slotmachine} ALL MATCHING! WIN WIN")
         elif (a == b) or (a == c) or (b == c):
-            await ctx.send(f"{slotmachine} 2 in a row, you won! 🎉")
+            await ctx.send(f"{slotmachine} 2 matching, win!")
         else:
-            await ctx.send(f"{slotmachine} No match, you lost 😢")
+            await ctx.send(f"{slotmachine} none matching, you lost 😢")
+    
+    @commands.is_owner()
+    @commands.command()
+    async def addpremium(ctx, member : discord.Member):
+    	with open('premiumusers.json', 'w') as f:
+    		json.dump(member, f)
 
 
 def setup(bot):
-    bot.add_cog(Fun_Commands(bot))
+    bot.add_cog(fun(bot))
